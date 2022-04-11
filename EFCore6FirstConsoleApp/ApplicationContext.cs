5c10d9eeaa796ff99ed3ba5b9522425437b67837
nurmakhan_pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-04-11 09:06:45 +0600

    Add project files.

---
+++ EFCore6FirstConsoleApp/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore6FirstConsoleApp
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=EFCore6FirstConsoleApp.db");
+        }
+    }
+}
